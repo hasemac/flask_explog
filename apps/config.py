5f51cd94f093b8e21da7d3926bc3e1e0b8c78b59
hasemac <hasegawa@triam.kyushu-u.ac.jp>
2022-03-25 17:09:46 +0900

    modify config-file

--- apps/config.py
+++ apps/config.py
@@ -7,19 +7,20 @@ basedir = Path(__file__).parent.parent
 class BaseConfig:
     SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ"
     WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f"
-    #SQLALCHEMY_DATABASE_URI = "mysql://aiadmin:zeDm3HLcBx@192.168.0.122/explog"
-    SQLALCHEMY_DATABASE_URI = "mysql://aiadmin:zeDm3HLcBx@192.168.0.150/explog"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # これがないと警告が出るらしい
+    
+    
     
 # BaseConfigクラスを継承してLocalConfigクラスを作成する
 class LocalConfig(BaseConfig):
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG_TB_INTERCEPT_REDIRECTS = False # デバッグバーを表示させるときはFalse
+    SQLALCHEMY_DATABASE_URI = "mysql://aiadmin:zeDm3HLcBx@192.168.0.150/explog"
 
 # BaseConfigクラスを継承してTestingConfigクラスを作成する
 class TestingConfig(BaseConfig):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SQLALCHEMY_DATABASE_URI = "mysql://aiadmin:zeDm3HLcBx@192.168.0.122/explog"
+    DEBUG_TB_INTERCEPT_REDIRECTS = False # デバッグバーを表示させるときはFalse
+    SQLALCHEMY_ECHO = True # SQL文がコンソールに出力される。
+    
 # config辞書にマッピングする
 config = {
     "testing": TestingConfig,
